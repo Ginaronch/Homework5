d0ae8d962f73f6b410be3a4f8e6382eaacbe4984
Ginaronch <alexandr.yarosh1@gmail.com>
2025-02-28 11:38:55 +0100

    version 1.0

--- 5.1.py
+++ 5.1.py
@@ -9,7 +9,7 @@ elif any(element.isupper() for element in user_input):
     print(False)
 elif any(element in string.punctuation.replace("_", "") for element in user_input):
     print(False)
-elif user_input.count("_") > 1:
+elif user_input.count("_") > 1 and all(char == "_" for char in user_input):
     print(False)
 elif user_input in keyword.kwlist:
     print(False)
--- пробник.py
+++ пробник.py
@@ -1,7 +1,6 @@
 import string
 user_input = input()
-for char in string.punctuation:
-    user_input = user_input.replace(char, '')
-    if len(user_input) > 140:
-        user_input = user_input[:140]
-print(user_input)
+if user_input.count("_") > 1 and user_input.startswith("_") and user_input.endswith("_") and len(user_input) >= 1:
+    print(False)
+else:
+    print(True)
